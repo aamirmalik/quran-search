47cffc930dc6acc10b052ad39f93ecff6471817c
Aamir Malik <aamirm@gmail.com>
2022-01-18 08:25:27 -0800

    Adding quran.py and json file as a repo

---
+++ quran.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import json
+
+FILENAME='quran-simple.json'
+
+class quran():
+
+    def __init__(self):
+        fh=open(FILENAME, 'r')
+        self.quran = json.load(fh)
+
+    def get_surah(self, surah_num):
+        return(self.quran[surah_num-1]['aya'])
+
+    def get_num_surahs(self):
+        return(len(self.quran))
+
+    def get_ayah(self, surah_num, ayat_num):
+        surah=self.get_surah(surah_num)
+        return(surah[ayat_num-1]['@text'])
+
+    def get_num_ayat(self, surah_num):
+        surah=self.get_surah(surah_num)
+        return(len(surah[ayat_num-1]))
+
+    def get_word(self, surah_num, ayat_num, word_num):
+        ayat=self.get_ayah(surah_num, ayat_num)
+        words=ayat.split(' ')
+        return (words[word_num-1])
+
+    def get_num_words(self, surah_num, ayat_num, word_num):
+        ayat=self.get_ayah(surah_num, ayat_num)
+        words=ayat.split(' ')
+        return (len(words))
+
+    def analyze_ayat(self, surah_num, ayat_num):
+        a=self.get_ayah(surah_num, ayat_num)
+        for i,ch in enumerate(a):
+            print(f"{i}: {ch} {ch.encode('utf-8')}")
+
+    def analyze_word(self, surah_num, ayat_num, word_num):
+        w=self.get_word(surah_num, ayat_num, word_num)
+        for i,ch in enumerate(w):
+            print(f"{i}: {ch} {ch.encode('utf-8')}")
+
+
