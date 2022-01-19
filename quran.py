6b1929a92ea8b97fac9420ff826a0b7f8a56072e
Aamir Malik <aamirm@gmail.com>
2022-01-18 23:07:23 -0800

    Minor cosmetic fizes

--- quran.py
+++ quran.py
@@ -10,18 +10,22 @@ class quran():
         self.quran = json.load(fh)
 
     def get_surah(self, surah_num):
-        return(self.quran[surah_num-1]['aya'])
+        surah=self.quran[surah_num-1]['aya']
+        s=[]
+        for i,ayah in enumerate(surah):
+            s.append(ayah['@text'])
+        return s
 
     def get_num_surahs(self):
         return(len(self.quran))
 
     def get_ayah(self, surah_num, ayat_num):
         surah=self.get_surah(surah_num)
-        return(surah[ayat_num-1]['@text'])
+        return(surah[ayat_num-1])
 
     def get_num_ayat(self, surah_num):
         surah=self.get_surah(surah_num)
-        return(len(surah[ayat_num-1]))
+        return(len(surah))
 
     def get_word(self, surah_num, ayat_num, word_num):
         ayat=self.get_ayah(surah_num, ayat_num)
@@ -34,11 +38,13 @@ class quran():
         return (len(words))
 
     def analyze_ayat(self, surah_num, ayat_num):
+        print(f"Analyzing {self.get_ayah(surah_num, ayat_num)}")
         a=self.get_ayah(surah_num, ayat_num)
         for i,ch in enumerate(a):
             print(f"{i}: {ch} {ch.encode('utf-8')}")
 
     def analyze_word(self, surah_num, ayat_num, word_num):
+        print(f"Analyzing Surah:{surah_num} Ayah:{ayat_num} Word:{word_num} {self.get_word(surah_num, ayat_num, word_num)}")
         w=self.get_word(surah_num, ayat_num, word_num)
         for i,ch in enumerate(w):
             print(f"{i}: {ch} {ch.encode('utf-8')}")
